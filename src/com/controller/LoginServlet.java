1e54f1a36ea49190790052761b58eb2084471539
AE103_PC18 <AE103_PC18@Infva05521>
2018-11-22 12:31:03 +0530

    FirstDyp

---
+++ src/com/controller/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+				String uname=request.getParameter("uname");
+				String pass=request.getParameter("pass");
+				if(uname.equals("aditya") && pass.equals("learning"))
+				{
+					HttpSession session = request.getSession();
+					session.setAttribute("username", uname);
+					response.sendRedirect("Welcome.jsp");
+				}
+				else
+				{
+					response.sendRedirect("Login.jsp");
+				}
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+	}
+
+}
